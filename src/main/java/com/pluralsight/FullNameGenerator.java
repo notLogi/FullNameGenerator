58016947fb7d6864033b1af27b23a9f9948f1e01
notLogi <92322066+notLogi@users.noreply.github.com>
2025-09-29 14:45:38 -0400

    FullNameGenerator assignment finished.

--- src/main/java/com/pluralsight/FullNameGenerator.java
+++ src/main/java/com/pluralsight/FullNameGenerator.java
@@ -5,20 +5,24 @@ import java.util.Scanner;
 public class FullNameGenerator {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
-        System.out.println("Please enter your name: ");
+        System.out.println("Please enter your name:\n ");
 
         String firstName = getFirstName(myScanner);
+        firstName = firstName.trim();
+
 
         System.out.println("Middle name: ");
         String middleName;
         middleName = myScanner.nextLine();
         if(!middleName.isEmpty()){
-            middleName = middleName.charAt(0) +  ". ";
+            middleName = middleName.toUpperCase().charAt(0) +  ". ";
         }
         String lastName = getLastName(myScanner);
+        lastName = lastName.trim();
 
         System.out.println("Suffix: ");
         String suffix = myScanner.nextLine();
+        suffix = suffix.trim();
         if(!suffix.isEmpty()){
             suffix = ", " + suffix;
         }
